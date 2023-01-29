e0541b1ec0379d723e970b60bdc32a25bc8ff0c5
amcmanu3 <mcdeweykp@gmail.com>
2023-01-28 19:10:27 -0500

    Change file tree doesn't reload on upload/delete

--- app/classes/shared/helpers.py
+++ app/classes/shared/helpers.py
@@ -965,7 +965,7 @@ class Helpers:
             rel = os.path.join(folder, raw_filename)
             dpath = os.path.join(folder, filename)
             if os.path.isdir(rel):
-                output += f"""<li class="tree-item" data-path="{dpath}">
+                output += f"""<li id="{dpath}li" class="tree-item" data-path="{dpath}">
                     \n<div id="{dpath}" data-path="{dpath}" data-name="{filename}" class="tree-caret tree-ctx-item tree-folder">
                     <span id="{dpath}span" class="files-tree-title" data-path="{dpath}" data-name="{filename}" onclick="getDirView(event)">
                       <i style="color: var(--info);" class="far fa-folder"></i>
@@ -976,7 +976,7 @@ class Helpers:
                     \n"""
             else:
                 if filename != "crafty_managed.txt":
-                    output += f"""<li
+                    output += f"""<li id="{dpath}li"
                     class="d-block tree-ctx-item tree-file tree-item"
                     data-path="{dpath}"
                     data-name="{filename}"
@@ -1004,7 +1004,7 @@ class Helpers:
             dpath = os.path.join(folder, filename)
             rel = os.path.join(folder, raw_filename)
             if os.path.isdir(rel):
-                output += f"""<li class="tree-item" data-path="{dpath}">
+                output += f"""<li id="{dpath}li" class="tree-item" data-path="{dpath}">
                     \n<div id="{dpath}" data-path="{dpath}" data-name="{filename}" class="tree-caret tree-ctx-item tree-folder">
                     <span id="{dpath}span" class="files-tree-title" data-path="{dpath}" data-name="{filename}" onclick="getDirView(event)">
                       <i style="color: var(--info);" class="far fa-folder"></i>
@@ -1014,7 +1014,7 @@ class Helpers:
                     </div><li>"""
             else:
                 if filename != "crafty_managed.txt":
-                    output += f"""<li
+                    output += f"""<li id="{dpath}li"
                     class="d-block tree-ctx-item tree-file tree-item"
                     data-path="{dpath}"
                     data-name="{filename}"
