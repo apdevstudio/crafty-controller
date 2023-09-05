751beaa7eb18cbcebc5798d766fdf547558b64eb
Wout Bouckaert <woutbouckaert@gmail.com>
2023-09-04 20:44:44 -0600

    Replace all instances of bleach with nh3. Using latest version of nh3.

--- app/classes/web/panel_handler.py
+++ app/classes/web/panel_handler.py
@@ -7,7 +7,7 @@ import json
 import logging
 import threading
 import urllib.parse
-import bleach
+import nh3
 import requests
 import tornado.web
 import tornado.escape
@@ -67,7 +67,7 @@ class PanelHandler(BaseHandler):
         ) in self.controller.crafty_perms.list_defined_crafty_permissions():
             argument = int(
                 float(
-                    bleach.clean(
+                    nh3.clean(
                         self.get_argument(f"permission_{permission.name}", "0")
                     )
                 )
@@ -79,7 +79,7 @@ class PanelHandler(BaseHandler):
 
             q_argument = int(
                 float(
-                    bleach.clean(self.get_argument(f"quantity_{permission.name}", "0"))
+                    nh3.clean(self.get_argument(f"quantity_{permission.name}", "0"))
                 )
             )
             if q_argument:
@@ -479,7 +479,7 @@ class PanelHandler(BaseHandler):
             template = "panel/dashboard.html"
 
         elif page == "server_detail":
-            subpage = bleach.clean(self.get_argument("subpage", ""))
+            subpage = nh3.clean(self.get_argument("subpage", ""))
 
             server_id = self.check_server_id()
             if server_id is None:
@@ -1284,7 +1284,7 @@ class PanelHandler(BaseHandler):
             template = "panel/panel_edit_user_apikeys.html"
 
         elif page == "remove_user":
-            user_id = bleach.clean(self.get_argument("id", None))
+            user_id = nh3.clean(self.get_argument("id", None))
 
             if (
                 not superuser
