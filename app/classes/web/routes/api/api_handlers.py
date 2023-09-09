f2b906fe5ce37f7711948fbdb5f09b17d1a4f387
Andrew <mcdeweykp@gmail.com>
2023-09-08 20:22:31 -0400

    Fix Refresh

--- app/classes/web/routes/api/api_handlers.py
+++ app/classes/web/routes/api/api_handlers.py
@@ -64,7 +64,10 @@ from app.classes.web.routes.api.crafty.config.server_dir import (
 )
 from app.classes.web.routes.api.crafty.clogs.index import ApiCraftyLogIndexHandler
 from app.classes.web.routes.api.crafty.imports.index import ApiImportFilesIndexHandler
-from app.classes.web.routes.api.crafty.exe_cache import ApiCraftyExeCacheIndexHandler
+from app.classes.web.routes.api.crafty.exe_cache import (
+    ApiCraftyJarCacheIndexHandler,
+    ApiCraftySteamCacheIndexHandler,
+)
 
 
 def api_handlers(handler_args):
@@ -173,8 +176,13 @@ def api_handlers(handler_args):
             handler_args,
         ),
         (
-            r"/api/v2/crafty/exeCache/?",
-            ApiCraftyExeCacheIndexHandler,
+            r"/api/v2/crafty/JarCache/?",
+            ApiCraftyJarCacheIndexHandler,
+            handler_args,
+        ),
+        (
+            r"/api/v2/crafty/SteamCache/?",
+            ApiCraftySteamCacheIndexHandler,
             handler_args,
         ),
         (
--- app/classes/web/routes/api/crafty/exe_cache.py
+++ app/classes/web/routes/api/crafty/exe_cache.py
@@ -1,7 +1,7 @@
 from app.classes.web.base_api_handler import BaseApiHandler
 
 
-class ApiCraftyExeCacheIndexHandler(BaseApiHandler):
+class ApiCraftyJarCacheIndexHandler(BaseApiHandler):
     def get(self):
         auth_data = self.authenticate_user()
         if not auth_data:
@@ -22,6 +22,32 @@ class ApiCraftyExeCacheIndexHandler(BaseApiHandler):
             200,
             {
                 "status": "ok",
-                "data": self.controller.server_jars.get_serverjar_data(),
+                "data": self.controller.server_jars.manual_refresh_cache(),
+            },
+        )
+
+
+class ApiCraftySteamCacheIndexHandler(BaseApiHandler):
+    def get(self):
+        auth_data = self.authenticate_user()
+        if not auth_data:
+            return
+        (
+            _,
+            _,
+            _,
+            _,
+            _,
+        ) = auth_data
+
+        if not auth_data[4]["superuser"]:
+            return self.finish_json(400, {"status": "error", "error": "NOT_AUTHORIZED"})
+
+        self.controller.server_jars.manual_refresh_cache()
+        self.finish_json(
+            200,
+            {
+                "status": "ok",
+                "data": self.controller.steam_apps.refresh_cache(),
             },
         )
